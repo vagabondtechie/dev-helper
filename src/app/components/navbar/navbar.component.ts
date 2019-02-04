7fcd71f1a9f1d96f57112c96a6e699a1c0bf5620
vagabondtechie <vagabondtechie@gmail.com>
2019-02-04 18:16:42 +0530

    Added the navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    styleUrls: ['./navbar.component.css'],
+    templateUrl: ''
+})
+
+export class NavbarComponent {
+
+}
