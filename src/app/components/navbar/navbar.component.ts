34f5ca23c36573195a1ff165e78ffe564c76ed87
vagabondtechie <vagabondtechie@gmail.com>
2019-02-05 16:24:02 +0530

    Minor fix - templateUrl

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-navbar',
     styleUrls: ['./navbar.component.css'],
-    templateUrl: ''
+    templateUrl: './navbar.component.html'
 })
 
 export class NavbarComponent {
