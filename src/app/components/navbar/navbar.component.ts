07dc4311412daaf7b6698d27474f94ec475753d7
vagabondtechie <vagabondtechie@gmail.com>
2019-10-23 18:43:18 +0530

    Cleaned up navbar component
    Changes:
     * Added logic to have one active navitem
     * Cleaned up redundant code in app.component
     * Generated new files for github pages

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
 })
 
 export class NavbarComponent {
+    navItems: any[];
 
+    constructor() {
+        this.navItems = [
+            { name: 'Paths', url: '/paths', active: true },
+            { name: 'Task Bucket', url: '/tasks', active: false },
+            { name: 'References', url: '/references', active: false },
+            { name: 'Utility functions', url: '/utilities', active: false }
+        ];
+    }
+
+    activate(item) {
+        this.navItems.forEach(i => i.active = false);
+        item.active = true;
+    }
 }
