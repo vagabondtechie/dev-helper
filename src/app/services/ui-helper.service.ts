45253528e10a26c33935ea2ba49d9542ac8d6bdc
vagabondtechie <vagabondtechie@gmail.com>
2020-01-11 17:45:29 +0530

    Corrected the vanishing message problem
    Issue:
     * When we click the copy to clipboard icon, quickly multiple times,
       the "Copied to clipboard" message did not persist for that long.
       It used to disappear within 2 seconds after the first click.

--- src/app/services/ui-helper.service.ts
+++ src/app/services/ui-helper.service.ts
@@ -11,8 +11,22 @@ export class UiHelperService {
 
   static showVanishingMessage(element: HTMLElement, message: string, showFor = 2000) {
     element.innerHTML = message;
+    if (!element.getAttribute("pending-to-vanish")) {
+      element.setAttribute("pending-to-vanish", "0");
+    }
+    let pendingToVanish = parseInt(element.getAttribute("pending-to-vanish"));
+    element.setAttribute("pending-to-vanish", "" + (pendingToVanish + 1));
     interval(showFor).pipe(
       take(1)
-    ).subscribe(_ => element.innerHTML = '');
+    ).subscribe(_ => {
+      let pendingToVanish = parseInt(element.getAttribute("pending-to-vanish"));
+      console.log(pendingToVanish);
+      if (pendingToVanish <= 1) {
+        element.innerHTML = '';
+        element.setAttribute("pending-to-vanish", "0");
+      } else {
+        element.setAttribute("pending-to-vanish", "" + (pendingToVanish - 1));
+      }
+    });
   }
 }
